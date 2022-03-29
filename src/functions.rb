528b1dc46c4d2e2f9d54bf74f27631d906ebaeac
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-03-29 12:54:10 +0800

    colorize welcome screen and refactor some of main code

--- src/functions.rb
+++ src/functions.rb
@@ -1,4 +1,6 @@
 # this module contains functions used in the dayplanner app
+require "tty-font"
+
 module Functions
     def self.print_add_confirmation(task_object)
         puts ""
@@ -7,6 +9,23 @@ module Functions
         puts "Description: #{task_object.description}"
         puts "Importance: #{task_object.importance}"
         puts "Due: #{task_object.due}"
+        puts ""
+    end
+
+    def self.print_welcome_screen
+        font = TTY::Font.new(:doom)
+        puts ""
+        title = font.write("braindump.").yellow
+        brain = "
+                         _.-'-'--._
+                       ,', ~'` ( .'`.
+                      ( ~'_ , .'(  >-)
+                     ( .-' (  `__.-<  )
+                      ( `-..--'_   .-')
+                       `(_( (-' `-'.-)
+                           `-.__.-'=/
+                              `._`='\n".magenta
+        print "#{brain}#{title}"
         puts ""
     end
 end
--- src/main.rb
+++ src/main.rb
@@ -1,48 +1,36 @@
-# TODOS
-# delete tasks method: what msg to output if deleting no tasks?
-
 # IMPORTS
 require "tty-prompt"
-require "tty-font"
 require "colorize"
 require "./functions"
 require "./task"
 require "./schedule"
 
 # CONSTANTS
-font = TTY::Font.new(:doom)
+# font = TTY::Font.new(:doom)
 prompt = TTY::Prompt.new
 storage_filepath = './schedule.json'
 
 # MAIN PROGRAM
 program_running = true
-puts ""
-puts font.write("braindump.")
-puts ""
+Functions.print_welcome_screen
 schedule = Schedule.new
 schedule.load_from_json(storage_filepath)
 
 while program_running
     puts ""
-    menu_selection = prompt.select("Menu selection".bold, ["Add task/s", "Delete task/s", "Mark as complete", "See schedule",
-                                                      "Clear schedule", "Quit"])
-
+    menu_selection = prompt.select("Menu selection".bold, ["Add task/s", "Delete task/s", "Mark as complete",
+                                                           "See schedule", "Clear schedule", "Quit"])
     case menu_selection
 
     when "Add task/s"
         anymore_tasks = true
         while anymore_tasks
-            # get task information from user
             description = prompt.ask("Enter task description.\n>>", required: true, max: 10)
             importance = prompt.select("How important is this task?", ["Low", "Medium", "High", "Very high"])
             due = prompt.select("When is this task due?", %w[Morning Midday Afternoon Evening])
-            # create task object
             task = Task.new(description, importance, due, false)
-            # add task to schedule
             schedule.add_task(task)
-            # print confirmation msg
             Functions.print_add_confirmation(task)
-            # ask user for anymore tasks
             anymore_tasks = prompt.yes?("Do you have another tasks to do today?")
         end
 
@@ -66,7 +54,8 @@ while program_running
         if schedule.task_list.empty?
             puts "#{'>>'.red} You currently have no tasks."
         else
-            completed_tasks = prompt.multi_select("Select task/s you've completed.", schedule.task_descriptions_completed)
+            completed_tasks = prompt.multi_select("Select task/s you've completed.",
+                                                  schedule.task_descriptions_completed)
             if completed_tasks.empty?
                 puts "#{'>>'.red} No tasks were selected."
             else
