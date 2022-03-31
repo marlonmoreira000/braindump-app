0cda641d0cc4fcf4647519d6480999108803d2b7
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-03-31 08:05:35 +0800

    print error msg  when ctrl+c is pressed during program

--- src/main.rb
+++ src/main.rb
@@ -10,98 +10,103 @@ font = TTY::Font.new(:doom)
 prompt = TTY::Prompt.new
 storage_filepath = './schedule.json'
 
-# MAIN PROGRAM
-program_running = true
-Functions.print_welcome_screen
-schedule = Schedule.new
-schedule.load_from_json(storage_filepath)
+begin
+    # MAIN PROGRAM
+    program_running = true
+    Functions.print_welcome_screen
+    schedule = Schedule.new
+    schedule.load_from_json(storage_filepath)
 
-while program_running
-    puts ""
-    menu_selection = prompt.select("MAIN MENU".bold, ["Add task/s", "Delete task/s", "Mark as complete",
-                                                           "See schedule", "Clear schedule", "Quit"])
-    case menu_selection
+    while program_running
+        puts ""
+        menu_selection = prompt.select("MAIN MENU".bold, ["Add task/s", "Delete task/s", "Mark as complete",
+                                                            "See schedule", "Clear schedule", "Quit"])
+        case menu_selection
 
-    when "Add task/s"
-        anymore_tasks = true
-        while anymore_tasks
-            begin
-                description = prompt.ask("Enter task description.\n>>", required: true)
-                raise StandardError if description.length > 50
-            rescue StandardError
-                puts "#{'>>'.red} Invalid input. Task decsription must be less than 50 characters."
-                retry
+        when "Add task/s"
+            anymore_tasks = true
+            while anymore_tasks
+                begin
+                    description = prompt.ask("Enter task description.\n>>", required: true)
+                    raise StandardError if description.length > 50
+                rescue StandardError
+                    puts "#{'>>'.red} Invalid input. Task decsription must be less than 50 characters."
+                    retry
+                end
+                importance = prompt.select("How important is this task?", ["Low", "Medium", "High", "Very high"])
+                due = prompt.select("When is this task due?", %w[Morning Midday Afternoon Evening])
+                task = Task.new(description, importance, due, false)
+                schedule.add_task(task)
+                Functions.print_add_confirmation(task)
+                anymore_tasks = prompt.yes?("Do you have another tasks to do today?")
             end
-            importance = prompt.select("How important is this task?", ["Low", "Medium", "High", "Very high"])
-            due = prompt.select("When is this task due?", %w[Morning Midday Afternoon Evening])
-            task = Task.new(description, importance, due, false)
-            schedule.add_task(task)
-            Functions.print_add_confirmation(task)
-            anymore_tasks = prompt.yes?("Do you have another tasks to do today?")
-        end
 
-    when "Delete task/s"
-        if schedule.task_list.empty?
-            puts "#{'>>'.red} You currently have no tasks."
-        else
-            tasks_to_delete = prompt.multi_select("Select task/s to delete.", schedule.task_descriptions)
-            if tasks_to_delete.empty?
-                puts "#{'>>'.red} No tasks were deleted."
+        when "Delete task/s"
+            if schedule.task_list.empty?
+                puts "#{'>>'.red} You currently have no tasks."
             else
-                confirm_delete = prompt.yes?("Are you sure you want to delete this task/s?")
-                if confirm_delete
-                    schedule.delete_tasks(tasks_to_delete)
-                    puts "#{'>>'.red} You deleted #{tasks_to_delete.length} task/s."
+                tasks_to_delete = prompt.multi_select("Select task/s to delete.", schedule.task_descriptions)
+                if tasks_to_delete.empty?
+                    puts "#{'>>'.red} No tasks were deleted."
+                else
+                    confirm_delete = prompt.yes?("Are you sure you want to delete this task/s?")
+                    if confirm_delete
+                        schedule.delete_tasks(tasks_to_delete)
+                        puts "#{'>>'.red} You deleted #{tasks_to_delete.length} task/s."
+                    end
                 end
             end
-        end
 
-    when "Mark as complete"
-        if schedule.task_list.empty?
-            puts "#{'>>'.red} You currently have no tasks."
-        elsif schedule.all_tasks_complete?
-            puts "#{'>>'.red} All tasks have been completed."
-        else
-            completed_tasks = prompt.multi_select("Select task/s you've completed.",
-                                                  schedule.task_descriptions_completed)
-            if completed_tasks.empty?
-                puts "#{'>>'.red} No tasks were selected."
+        when "Mark as complete"
+            if schedule.task_list.empty?
+                puts "#{'>>'.red} You currently have no tasks."
+            elsif schedule.all_tasks_complete?
+                puts "#{'>>'.red} All tasks have been completed."
             else
-                schedule.mark_tasks_as_complete(completed_tasks)
-                puts "#{'>>'.red} You comleted #{completed_tasks.length} task/s."
+                completed_tasks = prompt.multi_select("Select task/s you've completed.",
+                                                    schedule.task_descriptions_completed)
+                if completed_tasks.empty?
+                    puts "#{'>>'.red} No tasks were selected."
+                else
+                    schedule.mark_tasks_as_complete(completed_tasks)
+                    puts "#{'>>'.red} You comleted #{completed_tasks.length} task/s."
+                end
+
+                if schedule.all_tasks_complete?
+                    puts ""
+                    puts font.write("All  finished!").yellow
+                end
             end
 
-            if schedule.all_tasks_complete?
-                puts ""
-                puts font.write("All  finished!").yellow
+        when "See schedule"
+            if schedule.task_list.empty?
+                puts "#{'>>'.red} You currently have no tasks."
+            else
+                schedule.show_table
             end
-        end
 
-    when "See schedule"
-        if schedule.task_list.empty?
-            puts "#{'>>'.red} You currently have no tasks."
+        when "Clear schedule"
+            if schedule.task_list.empty?
+                puts "#{'>>'.red} You currently have no tasks."
+            else
+                confirm_delete = prompt.yes?("Are you sure you want to delete all tasks?")
+                if confirm_delete
+                    schedule.delete_all_tasks
+                    puts "#{'>>'.red} You deleted all tasks."
+                end
+            end
+
+        when "Quit"
+            puts "Goodbye!"
+            program_running = false
+            # update persistent storage with new task
+            schedule.update_storage(storage_filepath)
+
         else
-            schedule.show_table
+            puts "#{'>>'.red} I'm not sure how you arrived here but i'm scared :("
         end
-
-    when "Clear schedule"
-        if schedule.task_list.empty?
-            puts "#{'>>'.red} You currently have no tasks."
-        else
-            confirm_delete = prompt.yes?("Are you sure you want to delete all tasks?")
-            if confirm_delete
-                schedule.delete_all_tasks
-                puts "#{'>>'.red} You deleted all tasks."
-            end
-        end
-
-    when "Quit"
-        puts "Goodbye!"
-        program_running = false
-        # update persistent storage with new task
-        schedule.update_storage(storage_filepath)
-
-    else
-        puts "#{'>>'.red} I'm not sure how you arrived here but i'm scared :("
     end
+rescue TTY::Reader::InputInterrupt => e
+    puts ""
+    puts "Error (#{e.message.red}): program unexpectedly exited".red
 end
